3e11b3d2898ccddc96b3b1894c8144db422d7737
Cynthrus <cynthrus.achigwe@gmail.com>
2026-02-13 23:32:42 +0100

    Update

--- terraform/output.tf
+++ terraform/output.tf
@@ -18,3 +18,16 @@ output "vpc_id" {
 output "assets_bucket_name" {
   value = "project-bedrock-state-bucket"
 }
+
+
+# IAM - Developer user credentials
+output "dev_user_access_key_id" {
+  description = "Access key ID for bedrock-dev-view user"
+  value       = aws_iam_access_key.dev_view.id
+}
+
+output "dev_user_secret_access_key" {
+  description = "Secret access key for bedrock-dev-view user"
+  value       = aws_iam_access_key.dev_view.secret
+  sensitive   = true
+}
